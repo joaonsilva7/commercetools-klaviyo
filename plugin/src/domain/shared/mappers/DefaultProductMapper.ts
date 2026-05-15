de00e84749ea99da09142e77e2b68418ed493f78
João Silva - Central Lobão S.A <jsilva@CLAdmins-MacBook-Pro-4.local>
2026-05-15 19:21:14 +0100

    Add SKU to product URL in bulk import

--- plugin/src/domain/shared/mappers/DefaultProductMapper.ts
+++ plugin/src/domain/shared/mappers/DefaultProductMapper.ts
@@ -35,8 +35,9 @@ export class DefaultProductMapper implements ProductMapper {
         const productDescription = product.masterData.current.description;
         const productSlug = product.masterData.current.slug;
         const defaultProductSlug = getLocalizedStringAsText(productSlug);
+        const masterVariantSku = product.masterData.current.masterVariant.sku || "";
         const productUrl = process.env.PRODUCT_URL_TEMPLATE
-            ? String(process.env.PRODUCT_URL_TEMPLATE).replace('{{productSlug}}', defaultProductSlug)
+            ? String(process.env.PRODUCT_URL_TEMPLATE).replace('{{productSlug}}', defaultProductSlug).replace('{{sku}}', masterVariantSku)
             : 'None';
         const productMasterVariantImages = product.masterData.current.masterVariant.images;
         const allProductCategories = product.masterData.current.categories.concat(
@@ -118,8 +119,9 @@ export class DefaultProductMapper implements ProductMapper {
         const productDescription = product.masterData.current.description;
         const productSlug = product.masterData.current.slug;
         const defaultProductSlug = getLocalizedStringAsText(productSlug);
+        const masterVariantSku = product.masterData.current.masterVariant.sku || "";
         const productUrl = process.env.PRODUCT_URL_TEMPLATE
-            ? String(process.env.PRODUCT_URL_TEMPLATE).replace('{{productSlug}}', defaultProductSlug)
+            ? String(process.env.PRODUCT_URL_TEMPLATE).replace('{{productSlug}}', defaultProductSlug).replace('{{sku}}', masterVariantSku)
             : 'None';
         const variantImages = productVariant.images;
         const variantPrice = productVariant.prices
